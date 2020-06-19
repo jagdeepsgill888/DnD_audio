33e73b3c376f8cab2a633d7e69bf36b9980ea23e
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-06-19 12:03:46 -0400

    adding audio js that plays when clicked on image and control

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 (() => {
    console.log('fired');
 
-   let audioControls = document.querySelectorAll(".controls"),
+   let audioControls = document.querySelectorAll('.controls'),
     audio = document.querySelector('audio');
+    audioThumbs = document.querySelectorAll('.trackRef');
+
+    function loadAndPlay() {
+    // the "this" keywordd refers the image you have selected?
+      let currentTrack = `audio/${this.dataset.trackref}`;
+
+      audio.src = currentTrack;
+      // load method loads whatever resource you indicate
+      audio.load();
+
+      playAudio();
+
+    }
 
     function playAudio() {
       // play the audio tracks
@@ -18,4 +31,12 @@
 
     audioControls[0].addEventListener("click", playAudio);
     audioControls[1].addEventListener("click", rewindAudio);
+
+    // process the images tags and make them load the right audio tracks
+    for (thumb of audioThumbs) {
+      thumb.addEventListener('click', loadAndPlay);
+    }
+
+    // listen for the end of the audio track
+     audio.addEventListener('ended', () => console.log('track ended'));
 }) ();
